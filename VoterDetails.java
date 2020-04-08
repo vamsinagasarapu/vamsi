5178e17871a9698b98dc8b96fe62c456ed028eae
vamsinagasarapu <vamsi.nagasarapu@gmail.com>
2020-04-08 21:16:53 +0530

    committing the changes done in the file : VoterDetails.java

---
+++ VoterDetails.java
@@ -0,0 +1,27 @@
+package vamsi;
+
+public class VoterDetails {
+	
+	private String voterName;
+	private String voterID;
+	
+	public String getVoterName() {
+		return voterName;
+	}
+	public void setVoterName(String voterName) {
+		this.voterName = voterName;
+	}
+	public String getVoterID() {
+		return voterID;
+	}
+	public void setVoterID(String voterID) {
+		this.voterID = voterID;
+	}
+	
+	
+	void checkMethod() {
+		System.out.println("Inside the checker method ");
+
+	}
+	
+}
